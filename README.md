8e6b2e716a26f54069613b9d25649c67ddf8f13b
Goran Mekić <meka@tilda.center>
2021-01-23 13:27:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# FreeBSD OSS
+
+ * There is a software facing buffer (bs) and a hardware driver buffer (b)
+ * The sizes can be seen with `cat /dev/sndstat` as [b:_:_:_] [bs:_:_:_]
+ * OSS ioctl only concern software buffer fragments, not hardware
+ * Software buffer fragments don't matter, there is no quantization going on
+ * Hardware buffer fragments do matter for timing, but are inaccessible...
+
+Not sure how virtual_oss handles them, but I think it is processed blockwise,
+so try to match its "-s" parameter with Jack period and the block size of your
+hardware - what is it BTW?
+
+For USB the block size is according to buffer_ms sysctl, e.g. 2ms at 48kHz
+gives me 0.002*48000=96 samples per block, all multiples of this work well as
+Jack period.
+
+
+# Previous mail
+
+Actually it would be 2 fragments in the example that make up a total buffer
+size of 2*1024. Thus my patch would divide it into 8 fragments of size.
+*1024/8 = 256 samples instead.
+
+The problem lies not in the number of bytes written, but in the timing. Jack
+insists on writing / reading a certain number of samples at a time, one period
+per cycle. It is bound to do so in a fixed time frame, because Jack clients
+need input / output at a defined latency. OTOH, OSS driver also insists on
+reading / writing a certain number of samples at a time, one fragment full of
+samples. It is bound to do so in a fixed time frame, to avoid under- and
+overruns in communication with the hardware.
+
+Now both Jack and OSS have to meet each others timing constraints when reading
+/ writing the OSS buffer. The idea of a total buffer size that holds 2*period
+samples is to give some slack and allow Jack to be about one period late. I
+call this the jitter tolerance. But as shown in the example above, the jitter
+tolerance may be much less if there is a slight mismatch between the period
+and the samples per fragment.
+
+Jitter tolerance gets better if we can make either the period or the samples
+per fragment considerably smaller than the other. In our case that means we
+divide the total buffer size into smaller fragments, keeping overall latency
+at the same level.
+
+
+# Compiling
+
+```
+cc oss.c -o oss
+```
---
+++ oss.c
@@ -0,0 +1,106 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/soundcard.h>
+#include <unistd.h>
+
+
+static void checkError(const int value, const char *message)
+{
+  if (value == -1)
+  {
+    fprintf(stderr, "OSS error: %s %s\n", message, strerror(errno));
+    exit(1);
+  }
+}
+
+
+static int size2frag(int x)
+{
+  int frag = 0;
+  while ((1 << frag) < x) { ++frag; }
+  return frag;
+}
+
+
+int main()
+{
+  int channels = 2;
+  int devcaps;
+  int error;
+  int fd;
+  int format = AFMT_S32_NE;
+  int frag = 10;
+  int fragSize;
+  int samplerate = 48000;
+  int tmp;
+  int bufferSize = 1024;
+  int formatSize = sizeof(int32_t);
+  char *deviceName = "/dev/dsp";
+  oss_audioinfo ai;
+
+  fd = open(deviceName, O_RDWR, 0);
+  checkError(fd, "open");
+
+  ai.dev = -1;
+  error = ioctl(fd, SNDCTL_ENGINEINFO, &ai);
+  checkError(error, "SNDCTL_ENGINEINFO");
+  printf("min_channels: %d\n", ai.min_channels);
+  printf("max_channels: %d\n", ai.max_channels);
+  printf("latency: %d\n", ai.latency);
+  printf("handle: %s\n", ai.handle);
+  if (ai.min_rate > samplerate || samplerate > ai.max_rate)
+  {
+    fprintf(stderr, "%s doesn't support chosen ", deviceName);
+    fprintf(stderr, "samplerate of %dHz!\n", samplerate);
+    exit(1);
+  }
+
+  error = ioctl(fd, SNDCTL_DSP_GETCAPS, &devcaps);
+  checkError(error, "SNDCTL_DSP_GETCAPS");
+
+  tmp = channels;
+  error = ioctl(fd, SNDCTL_DSP_CHANNELS, &tmp);
+  checkError(error, "SNDCTL_DSP_CHANNELS");
+  if (tmp != channels)
+  {
+    fprintf(stderr, "%s doesn't support chosen ", deviceName);
+    fprintf(stderr, "channel count of %d", channels);
+    fprintf(stderr, ", set to %d!\n", tmp);
+  }
+  channels = tmp;
+
+  int minFrag = size2frag(formatSize * channels);
+  if (frag < minFrag)
+  {
+    int fsize = formatSize * 8;
+    fprintf(stderr, "Minimal fragmet size for %d channels", channels);
+    fprintf(stderr, " and %dbit sample size is %d!\n", fsize, minFrag);
+    exit(1);
+  }
+  frag = tmp;
+  error = ioctl(fd, SNDCTL_DSP_SETFRAGMENT, &tmp);
+  checkError(error, "SNDCTL_DSP_SETFRAGMENT");
+  if (tmp != frag) { fprintf(stderr, "Fragment size set to %d!\n", tmp); }
+  frag = tmp;
+
+  tmp = format;
+  error = ioctl(fd, SNDCTL_DSP_SETFMT, &tmp);
+  checkError(error, "SNDCTL_DSP_SETFMT");
+  if (tmp != AFMT_S32_NE && tmp != AFMT_S32_OE)
+  {
+    fprintf(stderr, "%s doesn't support chosen sample format!\n", deviceName);
+    exit(1);
+  }
+  format = tmp;
+
+  tmp = samplerate;
+  error = ioctl(fd, SNDCTL_DSP_SPEED, &tmp);
+  checkError(error, "SNDCTL_DSP_SPEED");
+
+  error = ioctl(fd, SNDCTL_DSP_GETBLKSIZE, &fragSize);
+  checkError(error, "SNDCTL_DSP_GETBLKSIZE");
+  return 0;
+}
